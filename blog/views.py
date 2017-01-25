2e7026c77c59c35a38ff83e07bf282d3a56c7f4d
Canet <akacanet87@gmail.com>
2017-01-25 17:55:28 +0900

    edit post_list templates again

--- blog/views.py
+++ blog/views.py
@@ -6,5 +6,5 @@ from .models import Post
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte).order_by('published_date')
-    return render(request, 'blog/post_list.html', {})
+    return render(request, 'blog/post_list.html', {'posts':posts})
 
